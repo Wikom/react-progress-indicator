df4299ef0e58b2a8d7d6a6cf5e19ab7ef29b96f0
rouvenWikom <r.diener@wikom.de>
2019-04-17 13:49:24 +0200

    Konfiguration und module Pfad geändert

--- production.config.js
+++ production.config.js
@@ -3,42 +3,26 @@
  */
 
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'lib'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
         library: 'reactProgressIndicator',
         libraryTarget: 'umd',
         umdNamedDefine: true
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader'
+                use: 'babel-loader'
             }
         ]
     },
     devtool: 'source-map',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                drop_console: true
-            },
-            output: {
-                comments: false
-            }
-        })
-    ],
     externals: {
         "prop-types": "prop-types",
         "react": "react",
